0bddee7e7a77898f164fcc4c23c7aaf6f8ab81b0
song52wow <619202904@qq.com>
2023-09-04 15:47:23 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import lottie from 'lottie-web'
+import {gsap} from 'gsap'
+import {ScrollTrigger} from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
+
+const mainRef = ref()
+const lottieRef = ref()
+
+onMounted(() => {
+  const lottieAnimation = lottie.loadAnimation({
+    container: lottieRef.value,
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    path: '/bloom.json'
+  });
+
+  ScrollTrigger.create({
+    trigger: mainRef.value,
+    start: 'top top',
+    end: 'bottom bottom',
+    scrub: true,
+    pin: true,
+    onUpdate(self) {
+      lottieAnimation.goToAndStop(self.progress * lottieAnimation.totalFrames, true)
+    } 
+  })
+})
+
+</script>
+
+<template>
+  <main ref="mainRef">
+    <div class="lottie" ref="lottieRef"></div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  height: 200vh;
+}
+
+.lottie {
+  width: 800px;
+}
+</style>
